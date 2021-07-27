7e6c8dc4ed2ef1f8537a32af5b05a9d8f3aa1feb
Elias Ylä-Jarkko <elias.yla-jarkko@live.fi>
2021-07-27 12:46:23 +0300

    Initialize ReferenceModel class

---
+++ prophet_projpred/__init__.py
@@ -0,0 +1,5 @@
+from . import models
+from .models import ReferenceModel
+
+
+__all__ = ["models"]
---
+++ prophet_projpred/models.py
@@ -0,0 +1,59 @@
+from prophet import Prophet
+import logging
+
+logger = logging.getLogger('prophet')
+
+
+class ReferenceModel(Prophet):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.variables = []
+        self.seasons = []
+
+    def add_regressor(self, name, prior_scale=None,
+                      standardize='auto', mode=None):
+        self.variables.append(
+            {'name': name,
+             'prior_scale': prior_scale})
+        super().add_regressor(name, prior_scale, standardize, mode)
+
+    def add_seasonality(self, name, period, fourier_order,
+                        prior_scale=None, **kwargs):
+        self.seasons.append(
+            {'name': name,
+             'period': period,
+             'fourier_order': fourier_order})
+        super().add_seasonality(name, period, fourier_order, prior_scale,
+                                **kwargs)
+
+    def projection_model(self, regressors=[]):
+        proj = Prophet(changepoint_prior_scale=0.01, holidays=self.holidays)
+        regressors = [variable for variable in self.variables if
+                      variable['name'] in regressors]
+
+        for season in self.seasons:
+            proj.add_seasonality(**season)
+
+        for reg in regressors:
+            proj.add_regressor(**reg)
+
+        return proj
+
+    def project(self, future, regressors):
+        future = future.copy()
+        submodel = self.projection_model(regressors)
+        try:
+            self.fit(future)
+        except Exception:
+            # Reference model is fitted. Proceed to projecting
+            pass
+        ref_forecast = self.predict(future)
+
+        # Project by fitting the submodel to reference model predictions
+        future['y'] = ref_forecast['yhat']
+        submodel.fit(future)
+        projection = submodel.predict(future)
+        return projection, submodel
+
+
+
