90d61b8dc5ef45300fd33ea330a69e0a43cf8f3d
Elias Ylä-Jarkko <elias.yla-jarkko@live.fi>
2021-08-10 12:26:52 +0300

    WIP adding metrics

--- prophet_projpred/family.py
+++ prophet_projpred/family.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from scipy.stats import norm
 import numpy as np
 
 
@@ -7,7 +8,21 @@ class Family(ABC):
     def kl(self, ref, sub):
         pass
 
+    @abstractmethod
+    def dispersion(self, ref, refvar, sub):
+        pass
+
+    @abstractmethod
+    def loglik(self, y, yhat, dis):
+        pass
+
 
 class Gaussian(Family):
     def kl(self, ref, sub, weights=1):
         return np.sum(weights * (sub - ref)**2)
+
+    def dispersion(self, ref, sigma_obs, sub):
+        return np.sqrt(sigma_obs + (ref - sub)**2)
+
+    def loglik(self, y, yhat, dis):
+        return np.sum(norm.logpdf(y, loc=yhat, scale=dis))
--- prophet_projpred/models.py
+++ prophet_projpred/models.py
@@ -2,6 +2,7 @@ from prophet import Prophet
 from .family import Gaussian
 import logging
 import pandas as pd
+import numpy as np
 
 logger = logging.getLogger('prophet')
 logger.setLevel(logging.WARNING)
@@ -30,11 +31,18 @@ class ReferenceModel(Prophet):
         super().add_seasonality(name, period, fourier_order, prior_scale,
                                 **kwargs)
 
-    def ref_pred(self, future, n_draws_pred):
-        return self.predictive_samples(future)['yhat'][:, n_draws_pred]
+    def ref_pred(self, future, ndraws_pred):
+        predictions = self.predictive_samples(future)['yhat'][:, 0:ndraws_pred]
+        predictions = pd.DataFrame(predictions)
+        predictions['ds'] = future['ds']
+        predictions = predictions.melt(id_vars=['ds'], var_name='draw',
+                                       value_name='yhat')
+        return predictions
 
     def ref_mean_pred(self, future):
-        return self.predict(future)['yhat']
+        prediction = self.predict(future).loc[:, ['ds', 'yhat']]
+        prediction['draw'] = 0
+        return prediction
 
     def projection_model(self, regressors=[]):
         proj = Prophet(changepoint_prior_scale=self.changepoint_prior_scale,
@@ -50,36 +58,70 @@ class ReferenceModel(Prophet):
 
         return proj
 
-    def project(self, future, regressors):
-        """Project mean of parameter draws to submodel space that is
-        spanned by the given regressors
+    def project(self, future, regressors, ndraws_pred=1):
+        """Project parameter draws to submodel space that is
+        spanned by the given regressors. Uses mean values from reference space
+        if only one draw is selected.
 
         :param future: DataFrame
         :param regressors: list
+        :param ndraws_pred: int
         :return: DataFrame, Prophet
         """
         future = future.copy()
-        submodel = self.projection_model(regressors)
         try:
             self.fit(future)
         except Exception:
             # Reference model is fitted. Proceed to projecting
             pass
 
+        if ndraws_pred > 1:
+            ref_pred_draws = self.ref_pred(future, ndraws_pred=ndraws_pred)
+        else:
+            ref_pred_draws = self.ref_mean_pred(future)
+
         # Project by fitting the submodel to reference model predictions
-        future['y'] = self.ref_mean_pred(future)
-        submodel.fit(future)
-        projection = submodel.predict(future)
-        return projection, submodel
+        predictions = pd.DataFrame()
+        for i in range(ndraws_pred):
+            y = ref_pred_draws.groupby(
+                'draw').get_group(i).loc[:, ['yhat']].set_index(future.index)
+            future['y'] = y
+            submodel = self.projection_model(regressors)
+            submodel.fit(future)
+            projection = submodel.predict(future).loc[:, ['ds', 'yhat']]
+            projection['draw'] = i
+            predictions = predictions.append(projection)
+        return predictions, submodel
 
     def search(self, future: pd.DataFrame):
         added_variables = []
-        path = []
+        path = {}
         while len(added_variables) < len(self.variables):
-            result, step = self.search_step(future, added_variables)
-            print(result)
-            added_variables.append(result.loc[0, 'variable'])
-            path.append(step)
+            kl_table, variables = self.search_step(future, added_variables)
+            print(kl_table)
+            # Add the variable which increases KL divergence the least
+            added_variables.append(kl_table.loc[0, 'variable'])
+
+            # Project draws from the reference to the previously selected
+            # submodel space and calculate mean predictions
+            predictions, _ = self.project(future, variables, ndraws_pred=10)
+            mean_predictions = predictions.groupby(
+                by='ds').aggregate('mean')
+            pred_indices = future.index.values > self.history.index.max()
+
+            # Calculate projection statistics
+            y_test = future.loc[pred_indices, 'y'].values
+            yhat_test = mean_predictions.loc[pred_indices, 'yhat'].values
+            mape = np.mean(np.abs((y_test - yhat_test)/y_test))
+
+            dis = self.family.dispersion()
+            elpd = self.family.loglik()
+
+            path[len(added_variables)] = {
+                'variables': variables,
+                'predictions': predictions,
+                'mape': mape
+            }
         return path
 
     def search_step(self, future: pd.DataFrame,
@@ -96,13 +138,11 @@ class ReferenceModel(Prophet):
         variables = [variable['name'] for variable in self.variables
                      if variable['name'] not in initial_variables]
         kl_divs = []
-        submodels = {}
         for var in variables:
             prediction, submodel = self.project(
                 future, initial_variables + [var])
-            kl = self.family.kl(ref_pred, prediction['yhat'])
+            kl = self.family.kl(ref_pred['yhat'], prediction['yhat'])
             kl_divs.append(kl)
-            submodels[var] = submodel
         result = pd.DataFrame(
             {'variable': variables, 'kl': kl_divs}
         ).sort_values(by='kl', ignore_index=True)
